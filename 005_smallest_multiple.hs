fda93302a004d24b53597c8e53997bda9a14164f
armand <armand.foucault@telecom-bretagne.eu>
2018-09-05 00:38:07 +0200

    Wrote prime and greatestPower functions

---
+++ 005_smallest_multiple.hs
@@ -0,0 +1,11 @@
+-- 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+-- What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+prime n
+    | n == 2    = True
+    | otherwise = all (\x -> n `rem` x /= 0) [s, (s-1)..2]
+                  where s = (floor $ sqrt $ fromIntegral n)
+
+greatestPower n boundary
+    | n > boundary      = 1
+    | otherwise         = n * greatestPower n (boundary `div` n)
